d6fc8ac11dfe50a558bba791d7ae60152bfff3d2
Klajdi Hoxha <klainti95@gmail.com>
2021-04-01 23:35:07 +0200

    html and ts of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FullCalendarComponent, CalendarOptions } from '@fullcalendar/angular';  
+import { EventInput } from '@fullcalendar/core';  
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-calendar';
+  
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth'
+  };
+  
+  // @ViewChild('fullcalendar', null) calendarComponent: FullCalendarComponent;
+  // options: any;
+  // eventsModel: any[] = [];
+  // calendarVisible = true;
+  // calendarWeekends = true;
+  // calendarEvents: EventInput[] = [
+  //   { title: 'Event Now', start: new Date() }
+  // ];
+  // toggleVisible(){
+  //   this.calendarVisible = !this.calendarVisible;
+  // }
+  // ngOnInit(){
+  //   this.options = {
+  //     editable: false,
+  //     disableDragging: false,
+  //     selectable: true,
+  //     theme: 'standart',
+  //     header: {
+  //       right: 'prev, next, today',
+  //       left: ''
+  //     },
+  //     validRange: {
+  //       start: '2017-05-01',
+  //       end: '2019-06-01'
+  //     },
+  //     plugins: [dayGridPlugin]
+  //   };
+  //   this.calendarEvents = [{
+  //     title: 'Updaten Event',
+  //     start: '2020-08',
+  //     end: '2020-10'
+  //   }];
+  // }
+  // toggleWeekends(){
+  //   this.calendarWeekends = !this.calendarWeekends;
+  // }
+  // eventClick(model){}
+  // dateClick(model){}
+  // eventDragStop(model){}
+  
+  // gotoPast(){
+  //   let calendarApi = this.calendarComponent.getApi();
+  //   calendarApi.gotoDate('2000-01-01');
+  // }
+
+  // handleDateClick(arg){
+  //   if(confirm('Would you like to add an event to ' + arg.dateStr + ' ?')){
+  //     this.calendarEvents = this.calendarEvents.concat({
+  //       title: 'New Event',
+  //       start: arg.date,
+  //       allDay: arg.allDay
+  //     })
+  //   }
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FullCalendarModule } from '@fullcalendar/angular';
+FullCalendarModule.registerPlugins([
+  dayGridPlugin
+]);
 
 @NgModule({
   declarations: [
